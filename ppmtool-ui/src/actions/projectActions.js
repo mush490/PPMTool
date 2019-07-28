ecd4a0ebb76c7c7c06e6782951a96c3f233bf56a
Michael Michaud <mfmichaud@gmail.com>
2019-07-28 13:22:15 -0400

    added get projects

--- ppmtool-ui/src/actions/projectActions.js
+++ ppmtool-ui/src/actions/projectActions.js
@@ -1,14 +1,23 @@
 import axios from "axios";
-import {GET_ERRORS} from "./types";
+import { GET_ERRORS, GET_PROJECTS } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
-    try {
-        const res = await axios.post("http://localhost:8080/api/projects", project);
-        history.push("dashboard");
-    } catch (error) {
-        dispatch({
-            type:GET_ERRORS,
-            payload:error.response.data
-        });
-    }
-}
+  try {
+    const res = await axios.post("http://localhost:8080/api/projects", project);
+    history.push("dashboard");
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data
+    });
+  }
+};
+
+export const getProjects = () => async dispatch => {
+  const res = await axios.get("http://localhost:8080/api/projects");
+
+  dispatch({
+    type: GET_PROJECTS,
+    payload: res.data
+  });
+};
